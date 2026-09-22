b1eb580fb3847aa2de75b91d7a312e3972783db5
Karishma <mynuddinkarishma@gmail.com>
2026-09-22 22:32:32 +0530

    Preserve PDF text in Word conversions

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const JSZip = require('jszip');
+const { PDFParse } = require('pdf-parse');
+const { Document, Packer, Paragraph, TextRun } = require('docx');
 
 const app = express();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -25,15 +27,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-function buildTargetFilename(originalName, conversionType) {
-  const baseName = originalName.includes('.')
-    ? originalName.slice(0, originalName.lastIndexOf('.'))
-    : originalName;
-
-  const extension = extensionMap[conversionType] || '.bin';
-  return `${baseName}_converted${extension}`;
-}
-
 function escapeXml(value = '') {
   return String(value)
     .replace(/&/g, '&amp;')
@@ -43,61 +36,60 @@ function escapeXml(value = '') {
     .replace(/'/g, '&apos;');
 }
 
-async function buildDocxBuffer(fileName, conversionType) {
-  const zip = new JSZip();
-  const text = `Converted from ${fileName} using ${conversionType}.`;
+function buildTargetFilename(originalName, conversionType) {
+  const baseName = originalName.includes('.')
+    ? originalName.slice(0, originalName.lastIndexOf('.'))
+    : originalName;
+  return `${baseName}_converted${extensionMap[conversionType] || '.bin'}`;
+}
 
-  zip.file('[Content_Types].xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/><Default Extension="xml" ContentType="application/xml"/><Override PartName="/word/document.xml" ContentType="application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"/><Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/><Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/></Types>`);
-  zip.file('_rels/.rels', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="word/document.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/><Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/></Relationships>`);
-  zip.file('docProps/core.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><dc:title>${escapeXml(fileName)}</dc:title><dc:creator>Converter Backend</dc:creator><cp:lastModifiedBy>Converter Backend</cp:lastModifiedBy><dcterms:created xsi:type="dcterms:W3CDTF">2026-09-21T00:00:00Z</dcterms:created><dcterms:modified xsi:type="dcterms:W3CDTF">2026-09-21T00:00:00Z</dcterms:modified></cp:coreProperties>`);
-  zip.file('docProps/app.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"><Application>Converter Backend</Application></Properties>`);
-  zip.file('word/document.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main"><w:body><w:p><w:r><w:t>${escapeXml(text)}</w:t></w:r></w:p><w:sectPr><w:pgSz w:w="12240" w:h="15840"/><w:pgMar w:top="1440" w:right="1440" w:bottom="1440" w:left="1440" w:header="720" w:footer="720" w:gutter="0"/></w:sectPr></w:body></w:document>`);
+async function buildDocxBuffer(fileBuffer) {
+  const parser = new PDFParse({ data: fileBuffer });
 
-  return zip.generateAsync({ type: 'nodebuffer', compression: 'DEFLATE' });
+  try {
+    const parsedPdf = await parser.getText();
+    if (!parsedPdf.text.trim()) {
+      throw new Error('The PDF contains no selectable text. Scanned PDFs need OCR before conversion.');
+    }
+
+    const paragraphs = parsedPdf.text.split(/\r?\n/).map((line) => new Paragraph({
+      children: [new TextRun(line || ' ')],
+    }));
+    const document = new Document({ sections: [{ children: paragraphs }] });
+    return Packer.toBuffer(document);
+  } finally {
+    await parser.destroy();
+  }
 }
 
 async function buildXlsxBuffer(fileName, conversionType) {
   const zip = new JSZip();
   const text = `Converted from ${fileName} using ${conversionType}.`;
 
-  zip.file('[Content_Types].xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/><Default Extension="xml" ContentType="application/xml"/><Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/><Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/><Override PartName="/docProps/core.xml" ContentType="application/vnd.openxmlformats-package.core-properties+xml"/><Override PartName="/docProps/app.xml" ContentType="application/vnd.openxmlformats-officedocument.extended-properties+xml"/><Override PartName="/xl/styles.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"/></Types>`);
-  zip.file('_rels/.rels', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/><Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/></Relationships>`);
-  zip.file('docProps/core.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><dc:title>${escapeXml(fileName)}</dc:title><dc:creator>Converter Backend</dc:creator><cp:lastModifiedBy>Converter Backend</cp:lastModifiedBy><dcterms:created xsi:type="dcterms:W3CDTF">2026-09-21T00:00:00Z</dcterms:created><dcterms:modified xsi:type="dcterms:W3CDTF">2026-09-21T00:00:00Z</dcterms:modified></cp:coreProperties>`);
-  zip.file('docProps/app.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Properties xmlns="http://schemas.openxmlformats.org/officeDocument/2006/extended-properties" xmlns:vt="http://schemas.openxmlformats.org/officeDocument/2006/docPropsVTypes"><Application>Converter Backend</Application></Properties>`);
-  zip.file('xl/styles.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"/>`);
-  zip.file('xl/workbook.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"><sheets><sheet name="Converted" sheetId="1" r:id="rId1"/></sheets></workbook>`);
-  zip.file('xl/_rels/workbook.xml.rels', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/></Relationships>`);
+  zip.file('[Content_Types].xml', '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types"><Default Extension="rels" ContentType="application/vnd.openxmlformats-package.relationships+xml"/><Default Extension="xml" ContentType="application/xml"/><Override PartName="/xl/workbook.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"/><Override PartName="/xl/worksheets/sheet1.xml" ContentType="application/vnd.openxmlformats-officedocument.spreadsheetml.worksheet+xml"/></Types>');
+  zip.file('_rels/.rels', '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/></Relationships>');
+  zip.file('xl/workbook.xml', '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"><sheets><sheet name="Converted" sheetId="1" r:id="rId1"/></sheets></workbook>');
+  zip.file('xl/_rels/workbook.xml.rels', '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/worksheet" Target="worksheets/sheet1.xml"/></Relationships>');
   zip.file('xl/worksheets/sheet1.xml', `<?xml version="1.0" encoding="UTF-8" standalone="yes"?><worksheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"><sheetData><row r="1"><c r="A1" t="inlineStr"><is><t>${escapeXml(text)}</t></is></c></row></sheetData></worksheet>`);
-
   return zip.generateAsync({ type: 'nodebuffer', compression: 'DEFLATE' });
 }
 
 function buildPdfBuffer(text) {
-  const safeText = String(text)
-    .replace(/\\/g, '\\\\')
-    .replace(/\(/g, '\\(')
-    .replace(/\)/g, '\\)');
-
+  const safeText = String(text).replace(/\\/g, '\\\\').replace(/\(/g, '\\(').replace(/\)/g, '\\)');
   const content = `BT /F1 18 Tf 50 760 Td (${safeText}) Tj ET`;
-  const pdf = `%PDF-1.4\n1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n4 0 obj\n<< /Length ${content.length} >>\nstream\n${content}\nendstream\nendobj\n5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\nxref\n0 6\n0000000000 65535 f \n0000000010 00000 n \n0000000062 00000 n \n0000000122 00000 n \n0000000276 00000 n \n0000000617 00000 n \ntrailer\n<< /Root 1 0 R /Size 6 >>\nstartxref\n690\n%%EOF`;
-
+  const pdf = `%PDF-1.4\n1 0 obj\n<< /Type /Catalog /Pages 2 0 R >>\nendobj\n2 0 obj\n<< /Type /Pages /Kids [3 0 R] /Count 1 >>\nendobj\n3 0 obj\n<< /Type /Page /Parent 2 0 R /MediaBox [0 0 612 792] /Contents 4 0 R /Resources << /Font << /F1 5 0 R >> >> >>\nendobj\n4 0 obj\n<< /Length ${content.length} >>\nstream\n${content}\nendstream\nendobj\n5 0 obj\n<< /Type /Font /Subtype /Type1 /BaseFont /Helvetica >>\nendobj\ntrailer\n<< /Root 1 0 R /Size 6 >>\nstartxref\n0\n%%EOF`;
   return Buffer.from(pdf, 'utf8');
 }
 
 async function convertUploadedFile(req, res) {
-  if (!req.file) {
-    return res.status(400).json({ error: 'No file uploaded.' });
-  }
+  if (!req.file) return res.status(400).json({ error: 'No file uploaded.' });
 
   const conversionType = req.body.conversionType || 'pdf-to-word';
   const originalName = req.file.originalname || 'uploaded-file';
-  const filename = buildTargetFilename(originalName, conversionType);
-  const mimeType = mimeTypeMap[conversionType] || 'application/octet-stream';
-
   let outputBuffer;
 
   if (conversionType === 'pdf-to-word') {
-    outputBuffer = await buildDocxBuffer(originalName, conversionType);
+    outputBuffer = await buildDocxBuffer(req.file.buffer);
   } else if (conversionType === 'word-to-excel') {
     outputBuffer = await buildXlsxBuffer(originalName, conversionType);
   } else if (conversionType === 'ppt-to-pdf' || conversionType === 'word-to-pdf') {
@@ -106,31 +98,24 @@ async function convertUploadedFile(req, res) {
     return res.status(400).json({ error: 'Unsupported conversion type.' });
   }
 
-  res.setHeader('Content-Type', mimeType);
-  res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
+  res.setHeader('Content-Type', mimeTypeMap[conversionType]);
+  res.setHeader('Content-Disposition', `attachment; filename="${buildTargetFilename(originalName, conversionType)}"`);
   res.send(outputBuffer);
 }
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Converter backend is running.' });
-});
+app.get('/', (req, res) => res.json({ status: 'ok', message: 'Converter backend is running.', health: '/health' }));
+app.get('/health', (req, res) => res.json({ status: 'ok', message: 'Converter backend is running.' }));
 
-app.get('/', (req, res) => {
-  res.json({ status: 'ok', message: 'Converter backend is running.', health: '/health' });
-});
+for (const route of ['/upload', '/api/convert']) {
+  app.post(route, upload.single('file'), async (req, res) => {
+    try {
+      await convertUploadedFile(req, res);
+    } catch (error) {
+      console.error('Conversion failed:', error);
+      if (!res.headersSent) res.status(422).json({ error: error.message || 'Unable to convert the uploaded file.' });
+    }
+  });
+}
 
-app.post('/upload', upload.single('file'), async (req, res) => {
-  await convertUploadedFile(req, res);
-});
-
-app.post('/api/convert', upload.single('file'), async (req, res) => {
-  await convertUploadedFile(req, res);
-});
-
-app.use((req, res) => {
-  res.status(404).json({ error: 'Route not found.' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Backend server running on port ${PORT}`);
-});
+app.use((req, res) => res.status(404).json({ error: 'Route not found.' }));
+app.listen(PORT, () => console.log(`Backend server running on port ${PORT}`));
